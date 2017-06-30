123e8f45bde4a011ea5c72b90a57138b8778178b
Dominique Feyer <dfeyer@ttree.ch>
2017-06-30 13:17:14 +0200

    TASK: Cleanup installer script

--- Classes/Composer/InstallerScripts.php
+++ Classes/Composer/InstallerScripts.php
@@ -1,16 +1,31 @@
 <?php
+
 namespace Ttree\FlowPlatformSh\Composer;
 
-use Composer\DependencyResolver\Operation\InstallOperation;
-use Composer\DependencyResolver\Operation\UpdateOperation;
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Plugin\PluginInterface;
 use Composer\Script\Event;
-use Composer\Installer\PackageEvent;
-use Neos\Flow\Package\PackageManager;
+use Composer\Script\ScriptEvents;
 use Neos\Utility\Files;
 
-class InstallerScripts
+class InstallerScripts implements PluginInterface, EventSubscriberInterface
 {
-    public static function postUpdateAndInstall(Event $event)
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        // Nothing to do here, as all features are provided through event listeners
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            ScriptEvents::POST_INSTALL_CMD => 'postUpdateAndInstall',
+            ScriptEvents::POST_UPDATE_CMD  => 'postUpdateAndInstall',
+        ];
+    }
+
+    public static function postUpdateAndInstall(Event $composerEvent)
     {
         if (!defined('FLOW_PATH_ROOT')) {
             define('FLOW_PATH_ROOT', Files::getUnixStylePath(getcwd()) . '/');
@@ -23,7 +38,9 @@ class InstallerScripts
         if (!defined('FLOW_PATH_CONFIGURATION')) {
             define('FLOW_PATH_CONFIGURATION', Files::getUnixStylePath(getcwd()) . '/Configuration/');
         }
+        $io = $composerEvent->getIO();
 
+        $io->write('<info>Platform.sh:</info>  Copy default configuration...');
         Files::copyDirectoryRecursively('Packages/Application/Ttree.FlowPlatformSh/Resources/Private/Installer/Distribution/Defaults', './', false, true);
     }
 }
